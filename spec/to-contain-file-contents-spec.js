0af1d450dbbfdf594e404a09dd8304e09f55c396
hulsea <andre.hulse@vizexplorer.com>
2016-07-11 17:30:10 +1200

    Increased coverage to 100 percent preceding refactor

--- spec/to-contain-file-contents-spec.js
+++ spec/to-contain-file-contents-spec.js
@@ -36,4 +36,24 @@ describe("Custom matcher toContainFileContents", function() {
             .not.toContainFileContents('a-menu-from-a-website', done);
     });
 
+    it('should not match when no file corresponding to the identifier exists', function (done) {
+        spyOn(done, 'fail').and.callFake(function(message) {
+            expect(message).toMatch("Error: ENOENT: no such file or directory, open '.+there-is-no-file-for-this-identifier\.txt'");
+            done();
+        });
+        expect('Configuration File Reference\nAPI\nStyle Guide\nSyntax vs JS Syntax\nSupport\nPlugins\nTimeouts\n'+
+            'Control Flow\nHow It Works\nUpgrading to Jasmine 2.x\nMobile Setup\nFAQ')
+            .toContainFileContents('there-is-no-file-for-this-identifier', done);
+    });
+
+    it('should fail negative compare when no file corresponding to the identifier exists', function (done) {
+        spyOn(done, 'fail').and.callFake(function(message) {
+            expect(message).toMatch("Error: ENOENT: no such file or directory, open '.+there-is-no-file-for-this-identifier\.txt'");
+            done();
+        });
+        expect('Configuration File Reference\nAPI\nStyle Guide\nSyntax vs JS Syntax\nSupport\nPlugins\nTimeouts\n'+
+            'Control Flow\nHow It Works\nUpgrading to Jasmine 2.x\nMobile Setup\nFAQ')
+            .not.toContainFileContents('there-is-no-file-for-this-identifier', done);
+    });
+
 });
