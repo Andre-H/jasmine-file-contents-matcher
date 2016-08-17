72fb731f0632626d6a070f422c3a4f92f9ef2270
hulsea <andre.hulse@vizexplorer.com>
2016-08-17 19:05:11 +1200

    Debug again

--- unmatched-file-util.js
+++ unmatched-file-util.js
@@ -1,25 +1,13 @@
 var fs = require('fs');
 
 exports.deleteDidNotMatchFile = function (fullFilePath){
-    if(fullFilePath == null){
-        return new Error("TypeError");
-    }
     try{
-        console.log('fullFilePath:'+fullFilePath);
         var failFilePath = fullFilePath.replace('.txt', '.didnotmatch.txt');
-
-        console.log('failFilePath:'+failFilePath);
-        console.log('fs:'+fs);
-        console.log('fs.unlinkSync:'+fs.unlinkSync);
-
         fs.unlinkSync(failFilePath);
         return 0;
     }
     catch (e){
-        console.log('e:'+e);
-        console.log('e.message:'+ e.message);
-        console.log('e.message.includes:'+e.message.includes);
-        if (!e.message.includes('ENOENT')) {
+        if (e.message && e.message.indexOf('ENOENT') > -1) {
             console.log('Tried to remove an unused placeholder file: ' + failFilePath + ', but an error occured:' + e +
                 '. This should not affect any test results.');
         }
